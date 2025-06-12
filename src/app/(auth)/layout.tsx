77337e188803f8d2975b947f4539d788f5769593
zrotrasukha <zrotrasukha@gmail.com>
2025-06-12 23:55:08 +0530

    Add AuthLayout component for authentication page layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <div className="flex min-h-screen flex-col items-center justify-center py-2">
+                <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                    {children}
+                </main>
+            </div>
+        </>
+    );
+}
+
+export default AuthLayout;
