389c19103b745b993c7d68e3917d70a1b74fdda8
zrotrasukha <zrotrasukha@gmail.com>
2025-06-12 23:55:38 +0530

    Add SignIn and SignUp pages with Clerk integration; implement UserButton in Home page

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+
+
+  return (
+    <div className="h-screen w-screen">
+      <UserButton />
+    </div>
+
+  );
+}
