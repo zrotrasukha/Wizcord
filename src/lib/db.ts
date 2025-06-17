f94284c6422e73fa76c6ba37a4a01bb498a5f64c
zrotrasukha <zrotrasukha@gmail.com>
2025-06-17 23:04:32 +0530

    feat: implement database connection using PrismaClient

---
+++ src/lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@/generated/prisma";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
+
+
+
+
