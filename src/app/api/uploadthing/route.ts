e9c6b520888eb625d142a8cda0c64316bf4ff7fe
zrotrasukha <zrotrasukha@gmail.com>
2025-06-17 23:05:49 +0530

    refactor: remove unused imports from upload route

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,8 +1,3 @@
-import { maxHeaderSize } from "node:http";
-import { FileRouter } from "uploadthing/types";
-import { f, handleAuth } from "./core";
-import { metadata } from "@/app/layout";
-
 import { uploadRouter } from "./core";
 import { createRouteHandler } from "uploadthing/next";
 
