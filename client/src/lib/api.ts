81234ae6214241a9fa7339d035f70dc97ae30f75
zrotrasukha <zrotrasukha@gmail.com>
2025-07-27 02:13:02 +0530

    feat: refactor createApi to use token parameter instead of async auth call

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,23 +1,10 @@
 import { hc } from 'hono/client';
 import type { appType } from "@server/app";
-import { useAuth } from '@clerk/clerk-react';
 
-// export const createApi = (token: string | null) => {
-//     return hc<appType>('http://localhost:4000/', {
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//         }
-//     }).api;
-// }
-// 
-
-export const createApi = async () => {
-    const auth = useAuth();
-    const token = await auth.getToken();
+export const createApi = (token: string | null) => {
     return hc<appType>('http://localhost:4000/', {
         headers: {
             Authorization: `Bearer ${token}`,
         }
     }).api;
-    
 }
