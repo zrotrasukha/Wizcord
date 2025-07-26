f960cd09f1823bd58675b6f59f771327055450cc
zrotrasukha <zrotrasukha@gmail.com>
2025-07-26 18:54:02 +0530

    feat: refactor createApi to use async token retrieval from useAuth

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,11 +1,23 @@
 import { hc } from 'hono/client';
 import type { appType } from "@server/app";
+import { useAuth } from '@clerk/clerk-react';
 
-export const createApi = (token: string | null) => {
+// export const createApi = (token: string | null) => {
+//     return hc<appType>('http://localhost:4000/', {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         }
+//     }).api;
+// }
+// 
+
+export const createApi = async () => {
+    const auth = useAuth();
+    const token = await auth.getToken();
     return hc<appType>('http://localhost:4000/', {
         headers: {
             Authorization: `Bearer ${token}`,
         }
     }).api;
+    
 }
-
