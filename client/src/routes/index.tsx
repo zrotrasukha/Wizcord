1434b83084b1a290a480200b5af6387c9f988f44
zrotrasukha <zrotrasukha@gmail.com>
2025-07-27 02:13:27 +0530

    feat: refactor RouteComponent to enhance UI with server creation and joining functionality

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,35 +1,70 @@
 import { createFileRoute } from '@tanstack/react-router'
-import '../index.css'
+import { Button } from '@/components/ui/button'
+import { Card, CardFooter, } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from 'react'
+import SelectFieldTab from '@/components/ui/selectFieldTab'
+import { createApi } from '@/lib/api'
 import { useAuth } from '@clerk/clerk-react'
-import { createApi } from '@/lib/api';
+// import {createServerSchema, type createServerType} from '../../../server/src/types/server.type'
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
-
 function RouteComponent() {
-  const auth = useAuth();
-  
-  const copyToken = async () => {
-    const token = await auth.getToken();
-    if (token) {
-      navigator.clipboard.writeText(token);
-      alert('Token copied to clipboard');
-    }
-    else {
-      alert('No token found');
-    }
-  }
-  
-  const getservers = async () => {
-    const api = createApi(await auth.getToken());
+  const [tabField, setTabField] = useState('create');
+  const [servername, setservername] = useState('');
+  const [joinServerLink, setJoinServerLink] = useState('');
+  const { getToken } = useAuth();
+
+  const handleCreateServer = async () => {
+    const api = createApi(await getToken());
+    const response = await api.server.create.$post({
+      json: {
+        name: servername
+      }
+    });
+    console.log(await response.json());
+  };
+  const getServers = async () => {
+    const api = createApi(await getToken());
+    // const response = await api.server.getservers.$get();
     const response = await api.server.getservers.$get();
     console.log(await response.json());
-  }
 
-  return <>
-    <div className="h-screen w-screen bg-blue-900 flex flex-col items-center justify-center ">
-      <button className='bg-blue-500 text-white p-2 rounded' onClick={copyToken}>Copy Token</button>
-      <button className='bg-blue-500 text-white p-2 rounded' onClick={getservers}>Test</button>
-    </div>
-  </>
+
+  }
+  return (
+    <div className='bg-zinc-900 h-screen flex items-center justify-center'>
+      <Card className='flex flex-col w-[400px] h-fit justify-center items-center'>
+        <Button onClick={getServers}>getServers</Button>
+        <SelectFieldTab value={tabField} onValueChange={setTabField} className='' />
+        {tabField === 'create' ? (
+          <div className='w-full px-4'>
+            <Label htmlFor='server-name' className='font-semibold  pb-2'>Please fill your legendary server name</Label>
+            <Input value={servername} onChange={(e) => setservername(e.target.value)} id='server-name' placeholder='Enter server name' className='italic' />
+            <CardFooter className='flex w-full items-center justify-end p-0 mt-2'>
+              <Button onClick={handleCreateServer}>Create</Button>
+            </CardFooter>
+          </div>
+        ) : (
+          <div className='w-full px-4'>
+            <Label htmlFor='join-link' className='font-semibold  pb-2'>Enter server invite link</Label>
+            <Input
+              value={joinServerLink}
+              onChange={(e) => setJoinServerLink(e.target.value.replace(/\s/g, ''))}
+              id='join-link'
+              placeholder='Enter invite link'
+              className='italic'
+            />
+            <CardFooter className='flex w-full items-center justify-end p-0 mt-2'>
+              <Button>Join</Button>
+            </CardFooter>
+          </div>
+        )
+        }
+      </Card >
+    </div >
+  )
 }
+
