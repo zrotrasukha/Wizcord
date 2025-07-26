c285c1468985647957817f38a213d508c3c0b686
zrotrasukha <zrotrasukha@gmail.com>
2025-07-26 14:50:25 +0530

    feat: implement getServers function to fetch user-specific servers

---
+++ server/src/service/server.service.ts
@@ -0,0 +1,25 @@
+import { db } from '@/src/db/db';
+import { server, serverMember } from '@/src/db/schemas/schema';
+import { eq, or } from 'drizzle-orm';
+
+
+export const getServers = async (userId:string) => {
+    const servers = await db
+        .select({
+            serverId: server.id,
+            name: server.name,
+            icon: server.icon,
+            admin: server.admin,
+            description: server.description,
+            createdAt: server.createdAt,
+            updatedAt: server.updatedAt
+        })
+        .from(server)
+        .leftJoin(serverMember, eq(serverMember.serverId, server.id))
+        .where(or(
+            eq(serverMember.userId, userId),
+            eq(server.admin, userId)
+        ))
+        .groupBy(server.id);
+   return servers; 
+}
