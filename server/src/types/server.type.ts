704e0d04582905db0b9c89f03cb68e623ab5f7bb
zrotrasukha <zrotrasukha@gmail.com>
2025-07-27 02:17:27 +0530

    feat: define createServerSchema and joinServerType for server creation validation

--- server/src/types/server.type.ts
+++ server/src/types/server.type.ts
@@ -1,2 +1,14 @@
+import {z} from "zod/v4";
+
 import { server } from "@/src/db/schemas/schema";
 export type serverType = typeof server.$inferSelect;
+
+export const joinServerType = z.url(); 
+
+export const createServerSchema = z.object({
+    name: z.string().min(1).max(225),
+    icon: z.url().optional(),
+    description: z.string().min(1).max(500).optional(),
+})
+
+export type createServerType = z.infer<typeof createServerSchema>;
