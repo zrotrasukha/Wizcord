b785ef39fee351a835dc147a956f65ef92429306
zrotrasukha <zrotrasukha@gmail.com>
2025-07-27 02:15:21 +0530

    feat: enhance server creation with user information retrieval and validation

--- server/src/controllers/server.controller.ts
+++ server/src/controllers/server.controller.ts
@@ -1,9 +1,13 @@
 import type { Context } from "hono";
 import { getServers } from "../service/server.service";
+import { ensureUserExists } from "../service/user.service";
 import { NOT_FOUND, OK } from "../utils/http-status-code";
 import type { createServerType, serverType } from "../types/server.type";
 import { server } from "../db/schemas/schema";
 import { db } from "../db/db";
+import { createClerkClient } from "@clerk/backend";
+
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
 
 export const getServersService = async (c: Context): Promise<Response> => {
     const userId = c.get("userId");
@@ -14,21 +18,30 @@ export const getServersService = async (c: Context): Promise<Response> => {
 export const createServerService = async (c: Context, body: createServerType): Promise<Response> => {
     const userId = c.get("userId");
     const { name, description, icon } = body;
+    const user = await clerkClient.users.getUser(userId);
+    const email = user.emailAddresses[0]?.emailAddress || '';
+    const username = user.username || `user_${userId.slice(-8)}`; // Fallback username
+    const avatar = user.imageUrl;
     
-    const createdServers: serverType[] = await db
+    const userInfo = {
+        username,
+        email,
+        avatar: avatar || ''
+    }
+    await ensureUserExists(userId, userInfo)
+
+    const createdServer: serverType[] = await db
         .insert(server)
         .values({
             name,
             admin: userId,
             description: description || '',
-            icon: icon || ''
-        })
-        .returning();
-    const createdServer = createdServers[0];
+            icon: icon || '',
+        }).returning();
 
-    if (!createdServer) {
+    if (!createdServer[0]) {
         return c.json({ error: "Failed to create server" }, NOT_FOUND);
     }
-    return c.json({ server: createdServer }, OK);
 
+    return c.json({ server: createdServer }, OK);
 }
