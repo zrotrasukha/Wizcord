cd0e39c669b6f8a936e52328c1010459a7f5e616
zrotrasukha <zrotrasukha@gmail.com>
2025-07-30 03:56:49 +0530

    refactor: simplify server creation response handling and remove unused getid route

--- server/src/routes/server.route.ts
+++ server/src/routes/server.route.ts
@@ -8,16 +8,6 @@ const serverHandler = new Hono()
   .get("/getservers", requireAuth, async (c) => getServersService(c))
   .post('/create', requireAuth, zv('json', createServerSchema), async (c) => {
     const body = c.req.valid('json');
-    const response = await createServerService(c, body).then(res => res.json());
-    return c.json({ response }, 201);
+    return await createServerService(c, body);
   })
-  .get("getid", requireAuth, async (c) => {
-    const userId = c.get("userId");
-    if (!userId) {
-      return c.json({ error: "User ID not found" }, 404);
-    }
-    return c.json({ userId }, 200);
-  })
-
-
 export default serverHandler;
