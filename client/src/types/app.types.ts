9a9c24f315ab3b6533644b4e3b2b72976240d259
zrotrasukha <zrotrasukha@gmail.com>
2025-07-30 03:56:43 +0530

    feat: add ServerType definition for server data structure

---
+++ client/src/types/app.types.ts
@@ -0,0 +1,10 @@
+
+export type ServerType = {
+    name: string;
+    icon: string | null;
+    description: string | null;
+    id: string;
+    createdAt: Date | null;
+    updatedAt: Date | null;
+    admin: string;
+}
