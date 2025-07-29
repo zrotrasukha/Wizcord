bda0b10e70d45393571a087607e59d61d967f8e6
zrotrasukha <zrotrasukha@gmail.com>
2025-07-29 14:52:52 +0530

    fix: update type imports and improve formatting in server controller

--- server/src/controllers/server.controller.ts
+++ server/src/controllers/server.controller.ts
@@ -2,7 +2,7 @@ import type { Context } from "hono";
 import { getServers } from "../service/server.service";
 import { ensureUserExists } from "../service/user.service";
 import { NOT_FOUND, OK } from "../utils/http-status-code";
-import type { createServerType, serverType } from "../types/server.type";
+import type { createServerType, ReturnServerType, ServerType } from "../types/server.type";
 import { server } from "../db/schemas/schema";
 import { db } from "../db/db";
 import { createClerkClient } from "@clerk/backend";
@@ -10,38 +10,45 @@ import { createClerkClient } from "@clerk/backend";
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
 
 export const getServersService = async (c: Context): Promise<Response> => {
-    const userId = c.get("userId");
-    const servers = await getServers(userId);
-    return c.json({ servers }, OK);
+  const userId = c.get("userId");
+  const servers = await getServers(userId);
+  return c.json({ servers }, OK);
 }
 
 export const createServerService = async (c: Context, body: createServerType): Promise<Response> => {
-    const userId = c.get("userId");
-    const { name, description, icon } = body;
-    const user = await clerkClient.users.getUser(userId);
-    const email = user.emailAddresses[0]?.emailAddress || '';
-    const username = user.username || `user_${userId.slice(-8)}`; // Fallback username
-    const avatar = user.imageUrl;
-    
-    const userInfo = {
-        username,
-        email,
-        avatar: avatar || ''
-    }
-    await ensureUserExists(userId, userInfo)
+  const userId = c.get("userId");
+  const { name, description, icon } = body;
+  const user = await clerkClient.users.getUser(userId);
+  const email = user.emailAddresses[0]?.emailAddress || '';
+  const username = user.username || `user_${userId.slice(-8)}`; // Fallback username
+  const avatar = user.imageUrl;
 
-    const createdServer: serverType[] = await db
-        .insert(server)
-        .values({
-            name,
-            admin: userId,
-            description: description || '',
-            icon: icon || '',
-        }).returning();
+  const userInfo = {
+    username,
+    email,
+    avatar: avatar || ''
+  }
+  await ensureUserExists(userId, userInfo)
 
-    if (!createdServer[0]) {
-        return c.json({ error: "Failed to create server" }, NOT_FOUND);
-    }
+  const createdServer: ReturnServerType[] = await db
+    .insert(server)
+    .values({
+      name,
+      admin: userId,
+      description: description || '',
+      icon: icon || '',
+    }).returning({
+      id: server.id,
+      name: server.name,
+      icon: server.icon,
+      description: server.description,
+      createdAt: server.createdAt,
+      updatedAt: server.updatedAt,
+    });
 
-    return c.json({ server: createdServer }, OK);
+  if (!createdServer[0]) {
+    return c.json({ error: "Failed to create server" }, NOT_FOUND);
+  }
+
+  return c.json({ server: createdServer }, OK);
 }
