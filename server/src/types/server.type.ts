8ac566585e0c2747b5cd948b39ec0c7006595b5b
zrotrasukha <zrotrasukha@gmail.com>
2025-07-29 14:53:16 +0530

    fix: standardize type naming and improve formatting in server type definitions

--- server/src/types/server.type.ts
+++ server/src/types/server.type.ts
@@ -1,14 +1,15 @@
-import {z} from "zod/v4";
+import { z } from "zod/v4";
 
 import { server } from "@/src/db/schemas/schema";
-export type serverType = typeof server.$inferSelect;
+export type ServerType = typeof server.$inferSelect;
+export type ReturnServerType = Omit<ServerType, 'name' | 'admin' | 'icon' | 'description'>;
 
-export const joinServerType = z.url(); 
+export const joinServerType = z.url();
 
 export const createServerSchema = z.object({
-    name: z.string().min(1).max(225),
-    icon: z.url().optional(),
-    description: z.string().min(1).max(500).optional(),
+  name: z.string().min(1).max(225),
+  icon: z.url().optional(),
+  description: z.string().min(1).max(500).optional(),
 })
 
 export type createServerType = z.infer<typeof createServerSchema>;
