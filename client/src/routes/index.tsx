41354c2d455db883151bca6a4f3fc9ac445e05b9
zrotrasukha <zrotrasukha@gmail.com>
2025-07-31 00:15:47 +0530

    feat: replace sidebar implementation with Sidebar and SecondSidebar components for improved layout

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
 import react, { useMemo } from 'react'
 import { useAuth } from '@clerk/clerk-react'
-import ServerDialogue from '@/components/ui/serverDialogue'
+import ServerDialogue from '@/components/serverDialogue'
 import { createApi } from '@/lib/api'
 import type { ServerType } from '@/types/app.types'
-import { FaCirclePlus as Plus } from "react-icons/fa6";
+import Sidebar from '@/components/sidebar'
+import SecondSidebar from '@/components/secondSidebar'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -87,39 +88,14 @@ function RouteComponent() {
             setShowServerDialogue={setShowServerDialogue}
           />
         ) : (
-          <div className='h-screen w-screen bg-zinc-950 flex items-center relative'>
-            {/* sidebar */}
-            <div className='py-2 absolute flex flex-col h-screen w-[65px] bg-zinc-600 left-0  overflow-hidden'>
-              {/* Plus icon */} 
-              <button className='mb-2 w-full h-14 bg-transparent flex items-center justify-center p-0 '
-                onClick={() => setShowServerDialogue(true)}>
-                <Plus size={42} className='text-white hover:text-blue-400 transition-all duration-200 cursor-pointer' />
-              </button>
-              <div className='h-[2px] w-3/4 place-self-center bg-white rounded-2xl mb-3 ' />
-              {/* server list*/}
-              <div>
-                {servers?.map((server) => {
-                  return (
-                    <div key={server.id} className='w-full h-14  flex items-center justify-center p-0 hover:bg-zinc-800 transition-colors cursor-pointer '>
-                      {server.icon ? (
-                        <img
-                          src={server.icon}
-                          alt={server.name}
-                          className='w-8 h-8 rounded-full object-cover'
-                        />
-                      ) : (
-                        <button className='w-12 h-12 bg-zinc-500 rounded-full flex items-center justify-center'>
-                          <span className='text-white text-xs font-bold'>
-                            {server.name.charAt(0).toUpperCase()}
-                          </span>
-                        </button>
-                      )}
-                    </div>
-                  )
-                })}
-              </div>
+          // sidebar
+          (
+            <div className='h-screen w-screen bg-zinc-950 flex relative'>
+              <Sidebar servers={servers} setShowServerDialogue={setShowServerDialogue} />
+              {/* second sidebar */}
+              <SecondSidebar server={servers![0]}/>
             </div>
-          </div>
+          )
         )}
       </TokenContext.Provider>
     </>
