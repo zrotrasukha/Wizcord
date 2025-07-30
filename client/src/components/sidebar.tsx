aef2d752b097fd876b438c1e3175549347927619
zrotrasukha <zrotrasukha@gmail.com>
2025-07-31 00:14:42 +0530

    feat: implement Sidebar component for server navigation and management

---
+++ client/src/components/sidebar.tsx
@@ -0,0 +1,26 @@
+import type { ServerType } from "@/types/app.types";
+import { FaCirclePlus as Plus } from "react-icons/fa6";
+import { ServerList } from '@/components/serverList'
+
+
+interface sideBarProps {
+    servers: ServerType[] | null;
+    setShowServerDialogue: React.Dispatch<React.SetStateAction<boolean>>;
+
+}
+
+export default function sidebar({ servers, setShowServerDialogue }: sideBarProps) {
+    return (
+
+        <div className='py-2 flex flex-col h-screen w-[65px] bg-zinc-600 left-0  overflow-hidden'>
+            {/* Plus icon */}
+            <button className='mb-2 w-full h-14 bg-transparent flex items-center justify-center p-0 '
+                onClick={() => setShowServerDialogue(true)}>
+                <Plus size={42} className='text-white hover:text-blue-400 transition-all duration-200 cursor-pointer' />
+            </button>
+            <div className='h-[2px] w-3/4 place-self-center bg-white rounded-2xl mb-3 ' />
+            {/* server list*/}
+            <ServerList servers={servers} />
+        </div>
+    )
+}
