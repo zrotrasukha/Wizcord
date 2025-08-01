428885ee00ab018643960618059250af6f71a01f
zrotrasukha <zrotrasukha@gmail.com>
2025-08-02 00:24:48 +0530

    feat: expand app types to include Category, ChannelType, and MessageType interfaces

--- client/src/types/app.types.ts
+++ client/src/types/app.types.ts
@@ -1,10 +1,33 @@
 
 export type ServerType = {
-    name: string;
-    icon: string | null;
-    description: string | null;
-    id: string;
-    createdAt: Date | null;
-    updatedAt: Date | null;
-    admin: string;
+  name: string;
+  icon: string | null;
+  description: string | null;
+  id: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  admin: string;
+}
+
+
+export interface Category {
+  id: string;
+  name: string;
+  serverId: string;
+}
+
+export interface ChannelType {
+  id: string;
+  name: string;
+  type: 'text' | 'voice';
+  serverId: string;
+  categoryId: string;
+}
+
+export interface MessageType {
+  id: string;
+  content: string;
+  author: string;
+  timestamp: Date;
+  channelId: string;
 }
