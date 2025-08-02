580334d94d403134ae80668596fc543d053c027f
zrotrasukha <zrotrasukha@gmail.com>
2025-08-03 03:30:58 +0530

    feat(types): add channelCreationContext to contextType for channel creation handling

--- client/src/types/context.type.ts
+++ client/src/types/context.type.ts
@@ -1,7 +1,13 @@
 
+export type channelCreateType = {
+  categoryId?: string;
+  serverId: string; 
+}
 export type contextType = {
   token: string;
   setToken: React.Dispatch<React.SetStateAction<string>>;
   showChannelCreateDialogue: boolean,
   setShowChannelCreateDialogue: React.Dispatch<React.SetStateAction<boolean>>
+  channelCreationContext: channelCreateType | null;
+  setChannelCreationContext : React.Dispatch<React.SetStateAction<channelCreateType | null>>;
 };
