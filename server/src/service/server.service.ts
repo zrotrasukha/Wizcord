c0231ca003fb79c645c50a16f3ff8d5df6aceb2f
zrotrasukha <zrotrasukha@gmail.com>
2025-08-03 16:01:46 +0530

    feat(server.service): add services(createServer, checkServerAccessForUser, getChannels)

--- server/src/service/server.service.ts
+++ server/src/service/server.service.ts
@@ -1,9 +1,8 @@
 import { db } from '@/src/db/db';
 import { server, serverMember } from '@/src/db/schemas/schema';
-import { eq, or } from 'drizzle-orm';
-
-
-
+import { and, eq, or } from 'drizzle-orm';
+import type { Context } from 'hono';
+import { NOT_FOUND } from '../utils/http-status-code';
 
 export const getServers = async (userId: string) => {
   const servers = await db
@@ -25,3 +24,57 @@ export const getServers = async (userId: string) => {
     .groupBy(server.id)
   return servers;
 }
+
+export const createServer = async (
+  userId: string,
+  body:
+    {
+      name: string;
+      description?: string;
+      icon?: string
+    }) => {
+
+  const createdServer = await db
+    .insert(server)
+    .values({
+      name: body.name,
+      admin: userId,
+      description: body.description || '',
+      icon: body.icon || '',
+    }).returning({
+      id: server.id,
+      name: server.name,
+      icon: server.icon,
+      description: server.description,
+      createdAt: server.createdAt,
+      updatedAt: server.updatedAt,
+    })
+  return createdServer;
+}
+export const checkServerAccessForUser =
+  async (serverId: string, userId: string)
+    : Promise<boolean> => {
+    const serverAccess = await db.select()
+      .from(serverMember)
+      .where(and(
+        eq(serverMember.serverId, serverId),
+        eq(serverMember.userId, userId)
+      ))
+      .limit(1);
+    if (serverAccess.length === 0) return false
+    return true;
+  }
+
+export const getChannels = async (serverId: string) => {
+  const channels = await db
+    .select()
+    .from(serverMember)
+    .where(eq(serverMember.serverId, serverId))
+    .limit(1);
+  if (!channels) {
+    return { error: "No channels found for this server" };
+  }
+}
+
+
+
