1ce55d8e31ac0278fecb15d55c3f81996fcfd6b7
zrotrasukha <zrotrasukha@gmail.com>
2025-08-03 12:56:36 +0530

    refactor(useAuth): Create useAuth for better DX and readibility of code

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,17 +1,23 @@
 import { useAuth } from "@clerk/clerk-react";
+import { useNavigate } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 
 export const useAuthToken = () => {
-    const { getToken, isSignedIn } = useAuth();
-    const [token, setToken] = useState<string>('');
+  const [token, setToken] = useState('');
+  const { getToken, isSignedIn } = useAuth();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchToken = async () => {
-            const token = await getToken();
-            setToken(token || '');
-        }
-        fetchToken();
-    }, [getToken, isSignedIn])
-    
-    return { token, setToken };
+  useEffect(() => {
+    if (!isSignedIn) {
+      navigate({ to: '/signin' });
+    }
+    const fetchToken = async () => {
+      const token = await getToken();
+      setToken(token || '');
+    }
+
+    fetchToken();
+  }, [getToken, isSignedIn, navigate])
+
+  return token
 }
