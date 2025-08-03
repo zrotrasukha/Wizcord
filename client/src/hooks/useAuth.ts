d18df43e1f66dc6a5614d879dc1e33e173e252c5
zrotrasukha <zrotrasukha@gmail.com>
2025-08-04 00:46:04 +0530

    refactor(useAuth): add loading state and error handling by adding trycatch block to fetchToken

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -3,21 +3,33 @@ import { useNavigate } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 
 export const useAuthToken = () => {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { getToken, isSignedIn } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isSignedIn) {
       navigate({ to: '/signin' });
+      setIsLoading(false);
+      return;
     }
+
     const fetchToken = async () => {
-      const token = await getToken();
-      setToken(token || '');
-    }
+      try {
+        setIsLoading(true);
+        const fetchedToken = await getToken({});
+        setToken(fetchedToken || '');
+      } catch (error) {
+        console.error('Error fetching token:', error);
+        setToken('');
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
     fetchToken();
-  }, [getToken, isSignedIn, navigate])
+  }, [getToken, isSignedIn, navigate]);
 
-  return token
+  return { token, setToken, isLoading, isSignedIn };
 }
