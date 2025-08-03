38cd915e89a7553013cff1a167056f1b694fbbcc
zrotrasukha <zrotrasukha@gmail.com>
2025-08-03 15:46:22 +0530

    refactor(hooks): update useServer to accept api prop

--- client/src/hooks/useServer.ts
+++ client/src/hooks/useServer.ts
@@ -1,53 +1,56 @@
-import { createApi } from "@/lib/api";
+import { type ApiType } from "@/lib/api";
 import type { ServerType } from "@/types/app.types";
-import { useCallback, useEffect, useMemo, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
-export const useServer = (token: string) => {
-    const [servers, setServers] = useState<ServerType[] | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
+interface useServerProps {
+  api: ApiType | null;
+  token: string;
+}
 
-    const api = useMemo(() => {
-        return token ? createApi(token) : null;
-    }, [token])
+export const useServer = ({ api, token }: useServerProps) => {
+  const [servers, setServers] = useState<ServerType[] | null>(null);
+  const [isServerLoading, setIsServerLoading] = useState(true);
+  const fetchServers = useCallback(async () => {
+    try {
+      if (!api) return [];
 
-    const fetchServers = useCallback(async () => {
-        try {
+      const serverRes = await api.server.getservers.$get();
+      if (!serverRes.ok) {
+        console.error('Failed to fetch servers:', serverRes.statusText);
+        return [];
+      }
 
-            if (!api) return [];
+      const data = (await serverRes.json()) as { Servers: ServerType[] };
+      if (data && typeof data === 'object' && 'Servers' in data && Array.isArray(data.Servers)) {
+        return data.Servers as ServerType[];
+      }
 
-            const serverRes = await api.server.getservers.$get();
-            if (!serverRes.ok) {
-                console.error('Failed to fetch servers:', serverRes.statusText);
-                return [];
-            }
-            const data = (await serverRes.json()) as { Servers: ServerType[] };
-            if (data && typeof data === 'object' && 'Servers' in data && Array.isArray(data)) {
-                return data.Servers as ServerType[];
-            }
-            console.error('Unexpected response format:', data);
-        } catch (error) {
-            console.error('Error fetching servers:', error);
-        }
-    }, [api])
+      console.error('Unexpected response format:', data);
+      return [];
+    } catch (error) {
+      console.error('Error fetching servers:', error);
+      return [];
+    }
+  }, [api])
 
-    useEffect(() => {
-        if (!token) return;
-        setIsLoading(true);
-        const loadServers = async () => {
-            const servers = await fetchServers();
-            setServers(servers as ServerType[]);
-            setIsLoading(false);
-        }
-        loadServers();
-    }, [token, fetchServers])
+  useEffect(() => {
+    if (!token) return;
+    setIsServerLoading(true);
+    const loadServers = async () => {
+      const servers = await fetchServers();
+      setServers(servers as ServerType[]);
+      setIsServerLoading(false);
+    }
+    loadServers();
+  }, [token, fetchServers])
 
-    const refreshServers = useCallback(async () => {
-        if (!token) return;
-        const servers = await fetchServers();
-        setServers(servers as ServerType[]);
-    }, [fetchServers])
+  const refreshServers = useCallback(async () => {
+    if (!token) return;
+    const servers = await fetchServers();
+    setServers(servers as ServerType[]);
+  }, [fetchServers])
 
 
-    return { servers, isLoading, refreshServers }
+  return { servers, isServerLoading, setServers, fetchServers, refreshServers }
 }
 
