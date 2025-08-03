e925352ddcc9294946576e8bc5a2191391195192
zrotrasukha <zrotrasukha@gmail.com>
2025-08-03 15:56:44 +0530

    refactor(server): update controller to use service methods, add getServerChannel controller

--- server/src/controllers/server.controller.ts
+++ server/src/controllers/server.controller.ts
@@ -1,21 +1,23 @@
 import type { Context } from "hono";
-import { getServers } from "../service/server.service";
+import * as ServerServices from "../service/server.service";
 import { ensureUserExists } from "../service/user.service";
 import { NOT_FOUND, OK } from "../utils/http-status-code";
-import type { createServerType, ReturnServerType, ServerType } from "../types/server.type";
-import { server } from "../db/schemas/schema";
+import type { createServerType, ReturnServerType } from "../types/server.type";
+import { server, serverMember } from "../db/schemas/schema";
 import { db } from "../db/db";
 import { createClerkClient } from "@clerk/backend";
+import { and, eq } from "drizzle-orm";
+import type { ChannelType } from "../../../client/src/types/app.types";
 
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
 
-export const getServersService = async (c: Context): Promise<Response> => {
+export const getServers = async (c: Context): Promise<Response> => {
   const userId = c.get("userId");
-  const servers = await getServers(userId);
+  const servers = await ServerServices.getServers(userId);
   return c.json({ servers }, OK);
 }
 
-export const createServerService = async (c: Context, body: createServerType): Promise<Response> => {
+export const createServer = async (c: Context, body: createServerType): Promise<Response> => {
   const userId = c.get("userId");
   const { name, description, icon } = body;
   const user = await clerkClient.users.getUser(userId);
@@ -30,25 +32,33 @@ export const createServerService = async (c: Context, body: createServerType): P
   }
   await ensureUserExists(userId, userInfo)
 
-  const createdServer: ReturnServerType[] = await db
-    .insert(server)
-    .values({
-      name,
-      admin: userId,
-      description: description || '',
-      icon: icon || '',
-    }).returning({
-      id: server.id,
-      name: server.name,
-      icon: server.icon,
-      description: server.description,
-      createdAt: server.createdAt,
-      updatedAt: server.updatedAt,
-    });
+  const createdServer: ReturnServerType[] = await ServerServices.createServer(userId, {
+    name,
+    description: description || '',
+    icon: icon || ''
+  });
 
   if (!createdServer[0]) {
     return c.json({ error: "Failed to create server" }, NOT_FOUND);
   }
 
   return c.json({ server: createdServer }, OK);
+}
+
+export const getServerChannels = async (c: Context, serverId: string)
+  : Promise<Response> => {
+  if (!serverId) {
+    return c.json({ error: "Server ID is required" }, NOT_FOUND);
+  }
+  // Ensure user has access to the server
+  const userId = c.get("userId");
+  const serverAccess = await ServerServices.checkServerAccessForUser(serverId, userId);
+  if (serverAccess) return c.json({ error: "Server Access denied" }, NOT_FOUND);
+
+  // Fetch channels for the server
+  const channels = ServerServices.getChannels(serverId);
+  if (!channels) {
+    return c.json({ error: "No channels found for this server" }, NOT_FOUND);
+  }
+  return c.json({ channels }, OK);
 }
