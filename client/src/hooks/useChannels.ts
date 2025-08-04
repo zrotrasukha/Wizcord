bf5ea14b4954d18a94f62641aef1bc784746d7d3
zrotrasukha <zrotrasukha@gmail.com>
2025-08-04 12:19:30 +0530

    refactor(useChannels): restructure hook to accept props object and remove unused fetch logic

--- client/src/hooks/useChannels.ts
+++ client/src/hooks/useChannels.ts
@@ -3,42 +3,20 @@ import { fakeChannels } from "@/lib/mock";
 import type { ChannelType } from "@/types/app.types";
 import { useCallback, useEffect, useState } from "react";
 
-export const useChannels = (api: ApiType, token: string) => {
+interface useChannelProps {
+  api: ApiType | null;
+  token: string;
+}
+//@ts-ignore
+export const useChannels = ({ api, token }: useChannelProps) => {
   const [channels, setChannels] = useState<ChannelType[]>(fakeChannels);
   const [loadingChannels, setLoadingChannels] = useState(false);
-
-  const fetchChannels = useCallback(async () => {
-    try {
-      const channels = await api.server.getservers.$get();
-      if (!channels.ok) {
-        console.error("Failed to fetch channels",);
-        return [];
-      }
-      const data = await channels.json();
-      if (data && typeof data === 'object' && 'channels' in data && Array.isArray(data.channels)) {
-        return data.channels as ChannelType[];
-      }
-      console.error("Unexpected response format:", data);
-      return [];
-    } catch (err) {
-      console.error("Error fetching channels:", err);
-      return [];
-    }
-  }, [api, token]);
-
-  useEffect(() => {
-    if (!token) return;
-    setLoadingChannels(true);
-    const loadChannels = async () => {
-      const fetchedChannels = await fetchChannels();
-      setChannels(fetchedChannels);
-      setLoadingChannels(false);
-    }
-    loadChannels();
-  }, [token, api]);
+  const [selectedChannel, setSelectedChannel] = useState<ChannelType | null>(null);
   return {
     channels,
     loadingChannels,
-    setChannels
+    setChannels,
+    selectedChannel,
+    setSelectedChannel,
   }
 }
