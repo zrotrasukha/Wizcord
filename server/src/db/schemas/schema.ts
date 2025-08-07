593cf2a3dad42622ae7989cd14a52f991aa55aac
zrotrasukha <zrotrasukha@gmail.com>
2025-08-07 18:52:36 +0530

    fix(schema): add onDelete cascade to serverId references in server_member, category, channel, and message tables

--- server/src/db/schemas/schema.ts
+++ server/src/db/schemas/schema.ts
@@ -30,20 +30,21 @@ export const server = pgTable(
 export const serverMember = pgTable(
   'server_member', {
   id: uuid('id').primaryKey().defaultRandom(),
-  serverId: uuid('serverId').notNull().references(() => server.id),
+  serverId: uuid('serverId').notNull().references(() => server.id, { onDelete: 'cascade' }),
   userId: text('userId').notNull().references(() => user.id),
   role: roleEnum().notNull().default('MEMBER'),
   joinedAt: timestamp('joinedAt', { withTimezone: true }).defaultNow(),
   updatedAt: timestamp('updatedAt', { withTimezone: true }).defaultNow()
 }
 )
+
 export const category = pgTable(
   'category',
   {
     id: uuid('id').primaryKey().defaultRandom(),
     visibility: visibility().notNull(),
     name: varchar('channelName', { length: 150 }),
-    serverId: uuid('serverId').references(() => server.id).notNull(),
+    serverId: uuid('serverId').references(() => server.id, { onDelete: 'cascade' }).notNull(),
     createdAt: timestamp('createdAt', { withTimezone: true }).defaultNow(),
     updatedAt: timestamp('updatedAt', { withTimezone: true }).defaultNow()
   }
@@ -55,8 +56,8 @@ export const channel = pgTable(
   name: varchar('name', { length: 100 }).notNull(),
   type: channelType().notNull(),
   visibility: visibility().notNull().default('public'),
-  serverId: uuid('serverId').references(() => server.id).notNull(),
-  categoryId: uuid('categoryId').references(() => category.id),
+  serverId: uuid('serverId').references(() => server.id, { onDelete: 'cascade' }).notNull(),
+  categoryId: uuid('categoryId').references(() => category.id, { onDelete: 'cascade' }),
   createdAt: timestamp('createdAt', { withTimezone: true }).defaultNow(),
   updatedAt: timestamp('updatedAt', { withTimezone: true }).defaultNow()
 }
@@ -66,8 +67,8 @@ export const message = pgTable(
   'messages', {
   id: uuid('id').primaryKey().defaultRandom(),
   content: text('content').notNull(),
-  serverId: uuid('serverId').notNull().references(() => server.id),
-  channelId: uuid('channelId').references(() => channel.id).notNull(),
+  serverId: uuid('serverId').notNull().references(() => server.id, { onDelete: 'cascade' }),
+  channelId: uuid('channelId').references(() => channel.id, { onDelete: 'cascade' }).notNull(),
   authorId: text('authorId').notNull().references(() => user.id),
   createdAt: timestamp('createdAt', { withTimezone: true }).defaultNow()
 }
