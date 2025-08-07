8930a8664321dbce5c1a745bcb3b099c1be9850b
zrotrasukha <zrotrasukha@gmail.com>
2025-08-07 18:55:07 +0530

    style(auth.middleware): format code for consistency and readability

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -2,12 +2,14 @@ import { getAuth } from "@hono/clerk-auth";
 import type { Context, Next } from "hono";
 import { UNAUTHORIZED } from "../utils/http-status-code";
 
-const requireAuth = async (c:Context, next: Next) => {
-    const auth = getAuth(c);
-    if(!auth?.userId){
-        return c.json({ error: "Unauthorized" }, UNAUTHORIZED);
-    }
-    c.set("userId", auth.userId);
-    return await next();
+const requireAuth = async (c: Context, next: Next) => {
+  const auth = getAuth(c);
+  if (!auth?.userId) {
+    return c.json({ error: "Unauthorized" }, UNAUTHORIZED);
+  }
+  c.set("userId", auth.userId);
+  return await next();
 }
+
 export default requireAuth;
+
