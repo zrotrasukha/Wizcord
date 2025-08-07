dd4a6f994ae7d5292a6dbb13bfb1348dc100a44c
zrotrasukha <zrotrasukha@gmail.com>
2025-08-07 18:50:12 +0530

    feat(migrations): add visibility column to channels and update foreign key constraints

---
+++ server/src/db/migrations/0003_huge_newton_destine.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "channels" ADD COLUMN "visibility" "visibility" DEFAULT 'public' NOT NULL;
---
+++ server/src/db/migrations/0005_good_eddie_brock.sql
@@ -0,0 +1,18 @@
+ALTER TABLE "category" DROP CONSTRAINT "category_serverId_server_id_fk";
+--> statement-breakpoint
+ALTER TABLE "channels" DROP CONSTRAINT "channels_serverId_server_id_fk";
+--> statement-breakpoint
+ALTER TABLE "channels" DROP CONSTRAINT "channels_categoryId_category_id_fk";
+--> statement-breakpoint
+ALTER TABLE "messages" DROP CONSTRAINT "messages_serverId_server_id_fk";
+--> statement-breakpoint
+ALTER TABLE "messages" DROP CONSTRAINT "messages_channelId_channels_id_fk";
+--> statement-breakpoint
+ALTER TABLE "server_member" DROP CONSTRAINT "server_member_serverId_server_id_fk";
+--> statement-breakpoint
+ALTER TABLE "category" ADD CONSTRAINT "category_serverId_server_id_fk" FOREIGN KEY ("serverId") REFERENCES "public"."server"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "channels" ADD CONSTRAINT "channels_serverId_server_id_fk" FOREIGN KEY ("serverId") REFERENCES "public"."server"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "channels" ADD CONSTRAINT "channels_categoryId_category_id_fk" FOREIGN KEY ("categoryId") REFERENCES "public"."category"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "messages" ADD CONSTRAINT "messages_serverId_server_id_fk" FOREIGN KEY ("serverId") REFERENCES "public"."server"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "messages" ADD CONSTRAINT "messages_channelId_channels_id_fk" FOREIGN KEY ("channelId") REFERENCES "public"."channels"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "server_member" ADD CONSTRAINT "server_member_serverId_server_id_fk" FOREIGN KEY ("serverId") REFERENCES "public"."server"("id") ON DELETE no action ON UPDATE no action;
