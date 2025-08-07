eb73303db15ca3618598ea4387bac469603c7316
zrotrasukha <zrotrasukha@gmail.com>
2025-08-07 18:49:48 +0530

    feat(channel): enhance createchannels function with error handling and permission checks

--- server/src/controllers/channel.controller.ts
+++ server/src/controllers/channel.controller.ts
@@ -1,8 +1,9 @@
 import * as ChannelServices from "@/src/service/channel.service"
 import * as ServerServices from "@/src/service/server.service";
-import { NOT_FOUND } from "../utils/http-status-code";
+import { BAD_REQUEST, CREATED, FORBIDDEN, INTERNAL_SERVER_ERROR, NOT_FOUND } from "../utils/http-status-code";
 import type { Context } from "hono";
 import { OK } from "../utils/http-status-code";
+import { type CreateChannelType } from "@shared/app.type";
 
 export const getServerChannels = async (c: Context)
   : Promise<Response> => {
@@ -21,4 +22,32 @@ export const getServerChannels = async (c: Context)
     return c.json({ error: "No channels found for this server" }, NOT_FOUND);
   }
   return c.json({ channels }, OK);
+}
+
+
+export const createchannels = async (c: Context, body: CreateChannelType) => {
+  const { serverId } = body;
+  try {
+    const userId = c.get("userId");
+    if (!userId) {
+      return c.json({ error: "User ID is required" }, BAD_REQUEST);
+    }
+    // check if user has access to the server
+    const hasPermission = await ChannelServices.checkChannelCreatePermission(serverId, userId);
+    if (!hasPermission) {
+      return c.json({ error: "You do not have permission to create channels in this server" }, FORBIDDEN);
+    }
+    // Create the channel
+    const newChannel = await ChannelServices.createChannel(c, body);
+    if (!newChannel) {
+      return c.json({ error: "Failed to create channel" }, INTERNAL_SERVER_ERROR);
+    }
+
+    return c.json({ newChannel }, CREATED);
+  }
+  catch (error) {
+    console.error("Error creating channel:", error);
+    return c.json({ error: "Internal server error" }, 500);
+  }
+
 }
