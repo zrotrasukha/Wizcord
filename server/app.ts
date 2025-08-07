dbb35be80baabd52826bef5f1aad346153783471
zrotrasukha <zrotrasukha@gmail.com>
2025-08-07 18:49:37 +0530

    feat(app): add category and channel routes to the API

--- server/app.ts
+++ server/app.ts
@@ -3,10 +3,14 @@ import { clerkMiddleware } from "@hono/clerk-auth"
 import { cors } from 'hono/cors'
 import serverHandler from "./src/routes/server.route";
 import onError from "./src/middlewares/error.middleware";
-import { pinoLogger } from 'hono-pino'
-import pino, { transport } from "pino";
+// import { pinoLogger } from 'hono-pino'
+// import pino, { transport } from "pino";
 import { logger } from 'hono/logger'
+import categoryHandler from "./src/routes/category.route";
+import channelHandler from "./src/routes/channel.route";
+import { createClerkClient } from "@clerk/backend";
 
+export const authClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
 const app = new Hono().basePath('/api/')
 app.use(logger());
 
@@ -30,6 +34,8 @@ app.use("*", clerkMiddleware());
 
 const route = app
   .route('/server', serverHandler)
+  .route('/category', categoryHandler)
+  .route('/channel', channelHandler)
   .get('/test', async (c) => {
     const a = 48 + 54;
     return c.json({ message: "Hello from Hono!", a }, 200);
