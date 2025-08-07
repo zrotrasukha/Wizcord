8c5e92488442f0cfb77e34bf1bbe18c46fba6592
zrotrasukha <zrotrasukha@gmail.com>
2025-08-07 19:04:52 +0530

    feat(channel): implement get all channels and create channel functionality with validation

--- server/src/routes/channel.route.ts
+++ server/src/routes/channel.route.ts
@@ -1,20 +1,39 @@
 import { Hono } from "hono";
 import * as ChannelService from "@/src/service/channel.service";
+import requireAuth from "../middlewares/auth.middleware";
+import { customZValidator as zv } from "../middlewares/zodValidator.middleware";
+import * as ChannelController from "@/src/controllers/channel.controller";
+import { CREATED, INTERNAL_SERVER_ERROR } from "../utils/http-status-code";
+import { createChannelSchema } from "../types/server.type";
+import { z } from "zod/v4";
+
+const getChannelSchema = z.object({
+  serverId: z.string().min(1, "Server ID is required")
+});
 
 const channelHandler = new Hono()
-  .get('/', async (c) => {
-    const body = await c.req.json();
-    const serverId = body.serverId as string;
+  .get('/all', requireAuth, async (c) => {
+    const serverIdArr = c.req.queries('serverId');
+    const serverId = Array.isArray(serverIdArr) ? serverIdArr[0] : serverIdArr;
+    console.log('lwda serverId', serverId);
+
     if (!serverId) {
-      return c.json({ error: "Server ID is required" }, 400);
+      return c.json({ error: "lwda nahi mila serverId" }, 400);
     }
     const channels = await ChannelService.getChannels(serverId);
     if (!channels) {
       return c.json([], 404);
     }
     return c.json({ channels }, 200);
-  });
+  })
+  .post('/create', requireAuth, zv('json', createChannelSchema), async (c) => {
+    const body = c.req.valid('json');
+    const channel = await ChannelController.createchannels(c, body);
+    if (!channel) {
+      return c.json({ error: "Failed to create channel" }, INTERNAL_SERVER_ERROR);
+    }
+    return c.json({ channel }, CREATED);
+  })
 
 
-
-export default channelHandler; 
+export default channelHandler;
